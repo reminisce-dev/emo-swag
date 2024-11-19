15c2ed276d99fe292437a4633e23ad4d644b3c5d
reminisce <189037156+reminisce-dev@users.noreply.github.com>
2024-11-19 12:01:30 -0800

    add avatar display

--- src/App.tsx
+++ src/App.tsx
@@ -20,42 +20,67 @@ function App() {
       }
 
       const data = await response.json();
-      console.log(data);
+      setPfp(data.avatar);
     } catch (error) {
       console.error("Error fetching profile:", error);
     }
   };
 
+  const refreshPage = () => {
+    setHandle("");
+    setPfp("");
+  };
+
   return (
     <div className="bg-[#000] flex flex-col justify-center items-center h-screen">
-      <h1 className="text-white font-mono text-3xl">Hello</h1>
+      <img
+        src="./logo.jpg"
+        alt="emo swag girl"
+        className="text-white w-[6em]"
+      ></img>
+      <h1 className="text-white font-mono text-3xl">emo-swag</h1>
       <div className="mt-4 home max-w-4xl mx-auto">
-        <div className="input-container">
-          <input
-            id="link-area"
-            spellCheck="false"
-            autoComplete="off"
-            autoCapitalize="off"
-            maxLength={256}
-            placeholder="input handle here"
-            aria-label="link input area"
-            data-form-type="text"
-            className="min-w-72 w-full px-4 py-2 font-mono text-sm text-[#fff] placeholder-[#fff] bg-black border border-gray-700 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-[#fff] focus:border-white-500"
-            value={handle}
-            onChange={(e) => setHandle(e.target.value)}
-          />
-        </div>
-        <div className="action-container flex justify-end font-mono text-white">
-          <button
-            className="search-btn bg-[#191919]
-            border border-gray-700 rounded-md
-             p-2 text-xs mt-2 hover:bg-[#1f1f1f]
-              hover:border-gray-700"
-            onClick={() => fetchBskyPfp(handle)}
-          >
-            search
-          </button>
-        </div>
+        {pfp ? (
+          <div className="flex flex-col items-center">
+            <img
+              src={pfp}
+              alt="Profile"
+              className="w-32 h-32 rounded-full border border-gray-700"
+            />
+            <button
+              className="mt-4 bg-[#191919] border border-gray-700 rounded-md p-2 text-xs text-white font-mono hover:bg-[#1f1f1f] hover:border-gray-700"
+              onClick={refreshPage}
+            >
+              find another
+            </button>
+          </div>
+        ) : (
+          <>
+            <div className="input-container">
+              <input
+                id="link-area"
+                spellCheck="false"
+                autoComplete="off"
+                autoCapitalize="off"
+                maxLength={256}
+                placeholder="reminisced.bsky.social"
+                aria-label="link input area"
+                data-form-type="text"
+                className="placeholder:text-[#333] min-w-72 w-full px-4 py-2 font-mono text-sm text-[#fff] placeholder-[#fff] bg-black border border-gray-700 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-[#fff] focus:border-white-500"
+                value={handle}
+                onChange={(e) => setHandle(e.target.value)}
+              />
+            </div>
+            <div className="action-container flex justify-end font-mono text-white">
+              <button
+                className="search-btn bg-[#191919] border border-gray-700 rounded-md p-2 text-xs mt-2 hover:bg-[#1f1f1f] hover:border-gray-700"
+                onClick={() => fetchBskyPfp(handle)}
+              >
+                search
+              </button>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
